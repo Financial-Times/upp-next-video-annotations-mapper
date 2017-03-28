a4d0718be5587e53c7232e40adc9fedc548ede76
Iulian Popa <iulian.popa@ft.com>
2017-03-28 10:40:53 +0300

    Add healthchecks unit tests.

--- healthchecks_test.go
+++ healthchecks_test.go
@@ -2,8 +2,56 @@ package main
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+	"net/http/httptest"
+	"net/http"
 )
 
-func TestCheckServiceAvailability(t *testing.T) {
-	// empty test
+var publicThingsAPIMock *httptest.Server
+
+func TestCheckPublicThingsAPIServiceAvailability(t *testing.T) {
+	startPublicThingsAPIServiceMock("happy")
+	defer stopServices()
+
+	result, err := checkServiceAvailability("publicThingsAPI", publicThingsAPIMock.URL + "/__health")
+	assert.Equal(t, result, "Ok")
+	assert.Nil(t, err)
+}
+
+func TestCheckPublicThingsAPIServiceNonAvailability(t *testing.T) {
+	startPublicThingsAPIServiceMock("notHappy")
+	defer stopServices()
+
+	_, err := checkServiceAvailability("publicThingsAPI", publicThingsAPIMock.URL + "/__health")
+	assert.Error(t, err)
+}
+
+func startPublicThingsAPIServiceMock(handlerType string) {
+	router := mux.NewRouter()
+	var health http.HandlerFunc
+
+	switch handlerType {
+	case "happy" :
+		health = happyHandler
+	case "notHappy" :
+		health = notHappyHandler
+	}
+
+	router.Path("/__health").Handler(handlers.MethodHandler{"GET": http.HandlerFunc(health)})
+
+	publicThingsAPIMock = httptest.NewServer(router)
+}
+
+func happyHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func notHappyHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func stopServices() {
+	publicThingsAPIMock.Close()
 }
