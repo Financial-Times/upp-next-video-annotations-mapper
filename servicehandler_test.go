acbad8a51c4212cbd978115420d61e3ee9a098f4
Iulian Popa <iulian.popa@ft.com>
2017-04-07 15:17:35 +0300

    Added unit tests for servicehandler.

---
+++ servicehandler_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func init() {
+	logger = newAppLogger("test")
+}
+
+func TestMapRequest(t *testing.T) {
+	startPublicThingsAPIMock(scenarioHappy)
+	defer stopService()
+
+	h := serviceHandler{
+		sc: serviceConfig{
+			publicThingsURI: publicThingsAPIURLMock(),
+		},
+	}
+
+	assert := assert.New(t)
+	tests := []struct {
+		fileName           string
+		expectedContent    string
+		expectedHttpStatus int
+	}{
+		{
+			"next-video-input.json",
+			newStringConceptSuggestion(t, "e2290d14-7e80-4db8-a715-949da4de9a07",
+				newSuggestion("http://api.ft.com/things/71a5efa5-e6e0-3ce1-9190-a7eac8bef325", "http://www.ft.com/ontology/Section", "isClassifiedBy", "Financials"),
+			),
+			http.StatusOK,
+		},
+		{
+			"next-video-invalid-anns-input.json",
+			"",
+			http.StatusBadRequest,
+		},
+	}
+
+	for _, test := range tests {
+		fileReader := getReader(test.fileName, t)
+		req, _ := http.NewRequest("POST", "http://next-video-annotaitons-mapper.ft.com/map", fileReader)
+		w := httptest.NewRecorder()
+
+		h.mapRequest(w, req)
+
+		body, err := ioutil.ReadAll(w.Body)
+
+		switch {
+		case err != nil:
+			assert.Fail(err.Error())
+		case test.expectedHttpStatus != http.StatusOK:
+			assert.Equal(test.expectedHttpStatus, http.StatusBadRequest, "HTTP status wrong. Input JSON: %s", test.fileName)
+		default:
+			assert.Equal(test.expectedHttpStatus, http.StatusOK, "HTTP status wrong. Input JSON: %s", test.fileName)
+			assert.Equal(test.expectedContent, string(body), "Marshalled content wrong. Input JSON: %s", test.fileName)
+		}
+	}
+}
+
+func getReader(fileName string, t *testing.T) *os.File {
+	file, err := os.Open("test-resources/" + fileName)
+	if err != nil {
+		assert.Fail(t, err.Error())
+		return nil
+	}
+
+	return file
+}
